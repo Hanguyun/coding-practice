611ebf6569a9858b49bb04b1cd606835971b927a
Hanguyun <gksrndbs1907@gmail.com>
2025-09-16 22:21:38 +0900

    C_Day4_1~3.c

---
+++ C_Day4_2.c
@@ -0,0 +1,53 @@
+/*#include<stdio.h> // 구구단을 출력하는 프로그램을 작성하시오.
+int main()
+{
+    int n, i = 1;
+
+    printf("정수를 입력하세요.:");
+    scanf("%d", &n);
+
+    for ( ; i <= 9; ++i)
+    {
+        printf("%d * %d = %d\n", n, i, n * i);
+    }
+
+
+    return 0;
+}
+*/
+/*#include<stdio.h> // 팩토리얼(n!)을 계산하는 프로그램을 작성하시오.
+int main()
+{
+    int n, pro = 1;
+
+    printf("정수를 입력하세요.:");
+    scanf("%d", &n);
+
+    for (int i = n; i > 0; i--)
+    {
+        printf("%d", i);
+
+        if (i > 1)
+            printf(" x ");
+        else
+            printf(" = ");
+
+        pro = pro * i;
+    }
+    printf("%d\n", pro);
+    return 0;
+}*/
+
+/*#include<stdio.h> // 정수 n을 입력 받아 1부터 n까지의 숫자를 차례대로 출력하는 프로그램을 작성하시오.
+int main()
+{
+    int n, i = 1;
+    printf("정수를 입력하세요. :");
+    scanf("%d", &n);
+
+    for ( ; i <= n; ++i)
+    {
+        printf("%d ", i);
+    }
+    return 0;
+} */
