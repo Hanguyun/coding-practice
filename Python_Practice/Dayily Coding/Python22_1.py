666766f4530906eac7a199c7c55fc2371712f7ab
Hanguyun <gksrndbs1907@gmail.com>
2025-11-05 12:16:29 +0900

    Day51
    Implement a Blackjack game with user and AI interactions, including card dealing, scoring, and game rules.

---
+++ Python_Practice/Dayily Coding/Python22_1.py
@@ -0,0 +1,131 @@
+'''
+- 다음과 같이 블랙잭 게임을 하는 프로그램을 만들어 보자
+1. 컴퓨터와 사용자에게 A,2,3,4,5,6,7,8,9,10,J,Q,K중 무작위로 카드를 2장씩 나누어 준다.
+2. 카드의 개수는 제한이 없다. 즉, A~K까지의 카드는 무제한으로 있다고 가정한다.
+3. 2~10의 카드는 카드의 숫자가 점수이고, J,Q,K는 10점으로 한다. A는 상황에 따라 1 또는 11로 계산이 가능하다.
+4. 컴퓨터가 받은 카드 중 첫 번째 카드는 사용자에게 공개한다.
+5. 사용자의 현재 점수를 계산하여 보여준다. 현재 점수가 21점 미만이면 카드를 더 받을지 아닐지 물어본다.
+6. 사용자가 카드를 그만 받으면 사용자의 현재 점수를 고정하고 컴퓨터의 카드 받기 진행
+7. 사용자가 카드를 더 받으면 카드를 추가로 받은 점수를 계산한다.
+    (21점이 넘어가면 사용자 패배. 21점이면 자동으로 카드를 그만 받는다. 그 이외의 경우 5번으로 돌아가 반복)
+8. 사용자가 카드 받기를 끝내면 컴퓨터의 카드 받기 시작
+9. 사용자가 22점 이상의 점수로 패배한 상황이면 컴퓨터는 자신의 점수만 공개하고 승리
+10. 사용자의 점수가 21점 이하면 컴퓨터는 카드를 공개하고 카드를 받기 시작한다.
+11. 컴퓨터는 17점 이상이 될 때까지 카드를 받는다.
+12. 컴퓨터가 21점 초과가 되면 사용자 승리
+13. 컴퓨터가 21점 이하 인데 컴퓨터와 사용자의 점수가 같다면 비김
+14. 컴퓨터가 21점 이하이면 컴퓨터와 사용자 중 점수가 높은 쪽이 승리
+15. 1~14의 과정을 게임을 그만하겠다고 할 때까지 반복
+'''
+import random
+import copy
+
+choice = ['A',2 ,3 ,4 ,5 ,6 ,7 ,8 ,9 ,'J','Q','K']
+result = []
+count = 0
+while True:
+    print(f"게임을 시작합니다.\n카드를 뽑겠습니다.")
+
+    ai = [random.choice(choice) for i in range(2)] # ai 카드를 choice에서 랜덤으로 2개 뽑기
+    user = [random.choice(choice) for i in range(2)] # user 카드를 choice에서 랜덤으로 2개 뽑기
+
+    print(f"AI 공개 카드:{ai[0]} user 카드:{user}")  # ai 카드 한 장 보여주고 user 카드를 모두 보여주기
+    if 'A' in user : # user 리스트 안에 A가 있는지 확인 후 실행
+        a = input(f"A가 뽑혔습니다. 1 또는 10으로 사용가능 합니다. 어떤 숫자를 사용하시겠습니까? 1 또는 10 둘 중 하나를 고르시오:")
+        if a == '10': # A를 리스트에서 삭제 후 10으로 변환
+            user.remove('A')
+            user.append('A: 10')
+        elif a == '1' : # A를 리스트에서 삭제 후 1로 변환
+            user.remove('A')
+            user.append('A: 1')
+
+    result_user = copy.deepcopy(user) # user리스트를 result에 깊은 복사를 이용하여 복사
+    result_ai = copy.deepcopy(ai)
+
+    if 'J' in result_user or 'J' in result_ai: # J,Q,K가 있다면 삭제하고 10 추가
+        if 'J' in result_user:
+            result_user.remove('J')
+            result_user.append(10)
+        if 'J' in result_ai:
+            result_ai.remove('J')
+            result_ai.append(10)
+    if 'Q' in result_user or 'Q' in result_ai:
+        if 'Q' in result_user:
+            result_user.remove('Q')
+            result_user.append(10)
+        if 'Q' in result_ai:
+            result_ai.remove('Q')
+            result_ai.append(10)
+    if 'K' in result_user or 'K' in result_ai:
+        if 'K' in result_user:
+            result_user.remove('K')
+            result_user.append(10)
+        if 'K' in result_ai:
+            result_ai.remove('J')
+            result_ai.append(10)
+
+    if 'A: 1' in result : # user리스트에 'A: 1'이 있다면 삭제하고 1 추가
+        result.remove('A: 1')
+        result.append(1)
+    elif 'A: 10' in result : # user리스트에 'A: 10'이 있다면 삭제하고 10 추가
+        result.remove('A: 10')
+        result.append(10)
+
+    result = sum(result)
+    print(f"AI 공개 카드:{ai[0]} user 카드:{user} 유저 합산:{result_user}") # ai 카드 한 장 보여주고 user 카드와 합산 보여주기
+    break
+
+while count == 5:
+    while True:
+        if result < 21 : # 21점 미만이면 게임을 더 할건지 안 할건지 묻기
+            go = int(input(f"카드를 한 장 더 뽑겠습니까? 1 카드 뽑기 0 게임 종료:"))
+            if go == 1 :
+                count += 1
+                ai_2 = [random.choice(choice)]  # ai 카드를 choice에서 랜덤으로 뽑기
+                user_2 = [random.choice(choice)]  # user 카드를 choice에서 랜덤으로 뽑기
+                ai = ai.append(ai_2) # ai가 추가로 뽑은 카드를 ai에 추가
+                user = user_2.append(user_2) # user가 추가로 뽑은 카드를 user에 추가
+#-----------------------------------------------------------------------------------------------            
+            elif 'A' in user:  # user 리스트 안에 A가 있는지 확인 후 실행
+                a = input(f"A가 뽑혔습니다. 1 또는 10으로 사용가능 합니다. 어떤 숫자를 사용하시겠습니까? 1 또는 10 둘 중 하나를 고르시오:")
+                if a == '10':  # A를 리스트에서 삭제 후 10으로 변환
+                    user.remove('A')
+                    user.append('A: 10')
+                elif a == '1':  # A를 리스트에서 삭제 후 1로 변환
+                    user.remove('A')
+                    user.append('A: 1')
+
+                if 'J' in result_user or 'J' in result_ai:  # J,Q,K가 있다면 삭제하고 10 추가
+                    if 'J' in result_user:
+                        result_user.remove('J')
+                        result_user.append(10)
+                    if 'J' in result_ai:
+                        result_ai.remove('J')
+                        result_ai.append(10)
+                if 'Q' in result_user or 'Q' in result_ai:
+                    if 'Q' in result_user:
+                        result_user.remove('Q')
+                        result_user.append(10)
+                    if 'Q' in result_ai:
+                        result_ai.remove('Q')
+                        result_ai.append(10)
+                if 'K' in result_user or 'K' in result_ai:
+                    if 'K' in result_user:
+                        result_user.remove('K')
+                        result_user.append(10)
+                    if 'K' in result_ai:
+                        result_ai.remove('J')
+                        result_ai.append(10)
+# -----------------------------------------------------------
+            elif go == 0 :
+                print(f"AI 카드:{ai} AI 합산:{result_ai} user 카드:{user} 유저 합산:{result_user}")
+            
+            elif result_user >= 22 :
+                    print(f"AI: {ai}로 AI 승리!")
+                    break
+            elif result_user == 21 :
+                    print(f"AI 카드:{ai} AI 합산:{result_ai} user 카드:{user} USER가 21점으로 승리")
+                    break
+        elif result_user <= 17
+            choice
+            
