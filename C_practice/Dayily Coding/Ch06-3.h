c5cf0e1a10f0180b39dc0ed48443fa16a9583b74
Hanguyun <gksrndbs1907@gmail.com>
2025-11-17 11:44:25 +0900

    Day56

--- C_practice/Dayily Coding/Ch06-3.h
+++ C_practice/Dayily Coding/Ch06-3.h
@@ -2,21 +2,19 @@ typedef struct DNode {
     Element data;
     struct DNode* prev;
     struct DNode* next;
-} DNode;
+}DNode;
 
 DNode org;
-
-DNode* alloc_node(Element e){
-    Node* p = (DNode*) malloc(sizeof(DNode));
-    p->data = e;
-    p->prev = NULL;
-    p->next = NULL:
+DNode* alloc_node(Element e) {
+    DNode* p = (DNode* )malloc(sizeof(DNode));
+    p -> data = e;
+    p -> next = NULL;
+    p -> prev = NULL;
     return p;
 }
 
-Element free_Node(DNode* p)
-{
-    Element e = p->data;
+Element free_node(DNode* p) {
+    Element e = p -> data;
     free(p);
     return e;
 }
@@ -26,87 +24,94 @@ void error(char* str){
     exit(1);
 }
 
-int is_empty() { return org.next == NULL;  }
-int is_full() {  return 0;  }
-void init_list() {  org.next = NULL;  }
+int is_empty() {return org.next == NULL;}
+int is_full() {return 0;}
+void init_list() {org.next = NULL;}
 
-DNode* get_Node(int pos){
-    DNode* p = org.next;
-    for(int i=0; i < pos; i++){
-        if(p == NULL) return NULL;
-    }
+DNode* get_node(int pos) {
+    DNode*p = org.next;
+    for (int i = 0; i < pos; i++, p->next)
+        if (p == NULL)
+            return NULL;
+
     return p;
 }
 
 Element get_entry(int pos) {
-    DNode* p= get_Node(pos);
+    DNode* p = get_node(pos);
     if(p == NULL)
-        error("해당 위치의 노드가 없어요\n");
+        error("해당 위치의 노드가 없습니다.\n");
     return p->data;
 }
 
 void insert(int pos, Element e) {
-    DNode* before = get_Node(pos-1);
-    if(before == NULL)
-            error("위치가 잘못됨\n");
-    DNode* p = alloc_node(e);
+    DNode* before;
+    if(pos != 0)
+        before = get_node(pos-1);
+        if(before==NULL)
+            error("위치가 잘못되었습니다.\n");
+    else {
+        before = &org;
+    }
+
+    DNode*p = alloc_node(e);
     p->next = before->next;
     p->prev = before;
     before->next = p;
-    if(p->next != NULL)
-            p->next->prev = p;
+    if(p->next!=NULL)
+        p->next->prev=p;
 }
 
-Element delete(int pos) {
-    if( is_empty())
-        error("빈 리스트에서 삭제는 불가능");
+Element delete(int pos){
+    if (is_empty())
+        error("리스트가 빈 상황에서 삭제할 수 없습니다.\n");
 
-    DNode* p = get_Node(pos);
-    if( p == NULL)
-        error("삭제할 위치의 노드가 없어요");
+    DNode* p= get_node(pos);
+
+    if(pos < 0 || p == NULL)
+        error("위치가 잘못되었습니다.\n");
 
     p->prev->next = p->next;
-    if(p->next != NULL)
-            p->next->prev = p->prev;
+    if (p->next != NULL)
+        p->next->prev = p->prev;
 
-    return free_Node(p);
+    return free_node(p);
 }
 
-void destory_list(){
-    while( is_empty() == 0 ) delete(0);
+void destory_list() {
+    while(is_empty() == 0) delete(0);
 }
 
-int size(){
+int size() {
     int count = 0;
-    for(DNode* p=org.next; p != NULL; p=p->next) count++;
+    for(DNode* p=org.next; p!=NULL; p=p->next) count++;
     return count;
 }
 
-void append(Element e){
-    insert( size(), e);
+void append(Element e) {
+    insert(size(), e);
 }
 
 Element pop() {
-    return delete(size()-1);
+    delete(size()-1);
 }
 
-void replace(int pos, Element e){
-    DNode* p = get_Node(pos);
-    if(p == NULL)
-        error("해당 위치의 노드가 없어요 \n");
-    p->data = e;
+void replace(int pos, Element e) {
+    DNode* p=get_node(pos);
+    if(p==NULL)
+        error("위치가 잘못 되었습니다.");
+    p->data=e;
 }
 
-int find2(int start, Element e){
+int find2(int start, Element e) {
     int i = start;
-    for(Node*p = get_Node(start); p != NULL ; p=p->next){
-        if(p->data == e )
-            return i;
-        i++;
+    for(DNode* p =get_node(start); p !=NULL; p=p->next, i++) {
+//        if(p->data == e)
+//            return i;
     }
     return -1;
 }
 
-int find1(Element e){
+int find1(Element e) {
     return find2(0, e);
 }
