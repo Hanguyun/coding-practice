0e604b2c83f8f73ff13ee3200e53e0dea1539aa6
Hanguyun <gksrndbs1907@gmail.com>
2026-01-17 17:19:36 +0900

    Day67

---
+++ Java/Java_Day1_1.java
@@ -0,0 +1,18 @@
+// 자바 출력 함수
+
+// print 개행 없이 출력
+// println 개행 출력
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    System.out.print("Hello2");
+    System.out.println("Hello3");
+    System.out.println("Hello4");
+  }
+}
+/*
+출력
+Hello World!
+  Hello2Hello3
+  Hello4
+  */
