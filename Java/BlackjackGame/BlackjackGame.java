58492ab320e1f5611994aa79827ab1211ff52240
Hanguyun <gksrndbs1907@gmail.com>
2026-03-26 14:52:45 +0900

    Day73

--- Java/BlackjackGame/BlackjackGame.java
+++ Java/BlackjackGame/BlackjackGame.java
@@ -28,43 +28,44 @@ public class Blackjack2 {
 			System.out.println("--------------------------------------------------");
 			
 			List<String> ai = new ArrayList<>(); // ai 패를 담을 리스트 생성
-			List<Integer> aiAchoice = new ArrayList<>(); // ai A 랜덤 선택을 위한 리스트 생성
-			aiAchoice.add(1); aiAchoice.add(11); // 리스트에 1과 11을 추가
 			int aiSum = 0; // ai 합계를 담을 변수 생성
 			Boolean aiBust = false;
+			String aiPick;
 			
 			List<String> user = new ArrayList<>(); // user 패를 담을 리스트 생성
 			String userPick; // user가 뽑은 카드를 담을 변수
 			int userSum = 0; // user 합계를 담을 변수 생성
 			Boolean userBust = false; 
 			
-			Collections.shuffle(card); // card 리스트 셔플
-			
-			String aiPick = card.get(r.nextInt(card.size()));
-			 /*card.size() card에 들어있는 원소 개수. 예를 들어 10개면 size()는 10.
-		     r.nextInt(card.size(()) 0 이상 ~ card.size()-1 이하 범위의 랜덤 정수 1개를 생성. 예) size가 10이면 0~9 중 하나.
-		     card.get(랜덤인덱스) card에 랜덤 인덱스 위치의 원소를 가져옴.
-		     Object aiPick = ... 뽑힌 값을 aiPick에 저장. */
-			ai.add(aiPick); // card 랜덤으로 뽑은 카드를 ai덱에 삽입
-			
-			if (aiPick.equals("A")) { // aiPick이 A라면 실행
-				if (aiSum > 10) { // aiSum이 10보다 크면
-					aiSum += 1; // +1
+			for(int i =0; i < 2; i++) { // Ai도 두 장 뽑고 시작
+				Collections.shuffle(card); // card 리스트 셔플
+				
+				aiPick = card.get(r.nextInt(card.size()));
+				 /*card.size() card에 들어있는 원소 개수. 예를 들어 10개면 size()는 10.
+			     r.nextInt(card.size(()) 0 이상 ~ card.size()-1 이하 범위의 랜덤 정수 1개를 생성. 예) size가 10이면 0~9 중 하나.
+			     card.get(랜덤인덱스) card에 랜덤 인덱스 위치의 원소를 가져옴.
+			     Object aiPick = ... 뽑힌 값을 aiPick에 저장. */
+				ai.add(aiPick); // card 랜덤으로 뽑은 카드를 ai덱에 삽입
+				
+				if (aiPick.equals("A")) { // aiPick이 A라면 실행
+					if (aiSum > 10) { // aiSum이 10보다 크면
+						aiSum += 1; // +1
+					}
+					else if (aiSum <= 10) { // aiSum이 10보다 작으면
+						aiSum += 11; // +11
+					}
 				}
-				else if (aiSum <= 10) { // aiSum이 10보다 작으면
-					aiSum += 11; // +11
+				else if (aiPick.equals("J") || aiPick.equals("Q") || aiPick.equals("K")) {
+					aiSum += 10; // aiPick에 J,Q,K가 있으면 aiSum에 10 추가
+				}
+				
+				else{ // aiPick이 A,J,Q,K가 아니라 숫자 String이면 Int로 바꾼 다음 userCount 리스트에 삽입
+					int intValue = Integer.parseInt(aiPick);
+					aiSum += intValue;
 				}
 			}
-			else if (aiPick.equals("J") || aiPick.equals("Q") || aiPick.equals("K")) {
-				aiSum += 10; // aiPick에 J,Q,K가 있으면 aiSum에 10 추가
-			}
 			
-			else{ // aiPick이 A,J,Q,K가 아니라 숫자 String이면 Int로 바꾼 다음 userCount 리스트에 삽입
-				int intValue = Integer.parseInt(aiPick);
-				aiSum += intValue;
-			}
-			
-			System.out.println("AI의 패 중 하나는 "+ai+"입니다.");
+			System.out.println("AI의 패 중 하나는 ["+(ai.get(0))+"]입니다."); // ai 패 두 개 중 하나만 출력
 			
 			// ----------------------------사용자 동작 로직------------------------------------
 			
@@ -99,7 +100,7 @@ public class Blackjack2 {
 			}
 			while (true) {
 				System.out.println("현재 당신의 패는 " + user + "입니다. 총합: " + userSum);
-				System.out.print("카드를 더 뽑으시겠습니까? 1 = Yes 0 = No :");
+				System.out.print("카드를 더 뽑으시겠습니까? (Yes=1 No=0):");
 	
 				int keepGoing = sc.nextInt();
 	
@@ -120,6 +121,9 @@ public class Blackjack2 {
 							user.remove("A"); // A를 지우고
 							user.add("A(11)"); // A(11)을 넣고
 							userSum += 11; // user 합계에 + 11
+						}
+						else {
+							System.out.println("잘못된 번호를 입력하셨습니다.");
 						}
 					}
 					
