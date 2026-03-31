5ddf69028a34d6439095ea0bb28e22add4f5750b
Hanguyun <gksrndbs1907@gmail.com>
2026-03-31 12:19:40 +0900

    Day75

---
+++ Java/Daily/Code0331_3.java
@@ -0,0 +1,22 @@
+package sample1;
+
+public class Code0331_3 {
+	
+	static void divide(int a, int b) throws ArithmeticException {
+		System.out.println(a / b);
+	}
+
+	public static void main(String[] args) {
+		try {
+		int num1 = Integer.parseInt(args[0]);
+		int num2 = Integer.parseInt(args[1]);
+			divide(num1, num2);
+		} catch(ArithmeticException e) {
+			System.out.println("0으로 나눌 수 없습니다.");			
+		} catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("인자값이 없습니다.");
+		}
+
+	}
+
+}
